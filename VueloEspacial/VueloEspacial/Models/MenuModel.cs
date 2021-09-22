0a3540f3d0442fc6d1da48048aeb7521042e0b7e
mCamaren0 <85297444+mCamaren0@users.noreply.github.com>
2021-09-21 21:05:04 -0600

    Add files via upload

---
+++ VueloEspacial/VueloEspacial/Models/MenuModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+namespace VueloEspacial.Models
+{
+    public class MenuModel
+    {
+      
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Icon { get; set; }
+
+        public MenuModel()
+        {
+        }
+    }
+}
